2d2336bca2835b8f745e295530882195883a0430
Hakim Ghods <ghods.hakim@hotmail.com>
2020-05-11 02:58:07 +0430

    name

--- MyApp/Controllers/TagController.cs
+++ MyApp/Controllers/TagController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MyApp.Data;
+using StackExchange.Redis;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,20 +10,23 @@ namespace MyApp.Controllers
 {
     public class TagController : Controller
     {
-        private readonly AppDbContext _db;
 
-        public TagController(AppDbContext db)
+        private readonly IDatabase _db;
+        public TagController()
         {
-            _db = db;
+
+
+            var redis = ConnectionMultiplexer.Connect("localhost:6379");
+            _db = redis.GetDatabase();
         }
 
         [Route("api/tag")]
         public IActionResult Get() =>
-             Ok(_db.Tags.Take(1000));
+             Ok(_db.HashGetAll("tags").Select(h=> new {Id=h.Name.ToString(),Name=h.Value.ToString() }));
 
         [Route("api/tag/{term}")]
         public IActionResult GetSearch(string term) =>
-             Ok(_db.Tags.Where(t=>t.Name.StartsWith(term)));
+             Ok(_db.HashGetAll("tags").Select(h => new { Id = h.Name.ToString(), Name = h.Value.ToString() }).Where(t => t.Name.StartsWith(term)));
 
 
     }
