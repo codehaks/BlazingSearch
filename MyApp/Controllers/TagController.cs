d4f69e3a45dc19c1e6c382bca4bf12a097baaf09
Hakim Ghods <ghods.hakim@hotmail.com>
2020-05-11 02:10:31 +0430

    fix

--- MyApp/Controllers/TagController.cs
+++ MyApp/Controllers/TagController.cs
@@ -18,7 +18,7 @@ namespace MyApp.Controllers
 
         [Route("api/tag")]
         public IActionResult Get() =>
-             Ok(_db.Tags.Take(2_000));
+             Ok(_db.Tags);
 
         [Route("api/tag/{term}")]
         public IActionResult GetSearch(string term) =>
