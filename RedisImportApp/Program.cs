71552919f21d7ffd1f3bf5e8a38d875ae4c59356
Hakim Ghods <ghods.hakim@hotmail.com>
2020-05-11 03:42:59 +0430

    Fix

--- RedisImportApp/Program.cs
+++ RedisImportApp/Program.cs
@@ -16,14 +16,23 @@ namespace RedisImportApp
             var names = System.IO.File.ReadAllLines(@"E:\Projects\Data\Firstnames.txt");
             var list = new List<HashEntry>();
             var counter = 1;
-            foreach (var item in names.Take(10000))
+
+            foreach (var item in names)
             {
                 list.Add(new HashEntry(new RedisValue(counter.ToString()), new RedisValue(item)));
                 counter++;
+
+                if (counter % 1000==0)
+                {
+                    Console.WriteLine(counter);
+                    db.HashSet(new RedisKey("tags"), list.ToArray());
+                    list= new List<HashEntry>();
+                }
             }
 
-            db.HashSet(new RedisKey("tags"),list.ToArray());
+            
 
+            Console.WriteLine("Done!");
         }
     }
 }
